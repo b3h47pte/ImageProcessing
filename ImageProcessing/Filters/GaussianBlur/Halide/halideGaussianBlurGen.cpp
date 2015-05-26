21f5e3b509c307ac8b53c9be27009e72efc5d606
Mike.Bao <halo343gs@gmail.com>
2015-05-26 00:06:47 -0700

    Hopefully this gaussian blur and convolution works...

--- ImageProcessing/Filters/GaussianBlur/Halide/halideGaussianBlurGen.cpp
+++ ImageProcessing/Filters/GaussianBlur/Halide/halideGaussianBlurGen.cpp
@@ -13,21 +13,24 @@ int main(int argc, char** argv) {
     // Implement Gaussian Blur like NVIDIA : http://http.developer.nvidia.com/GPUGems3/gpugems3_ch40.html
     // Gaussian Function
     Func gaussian;
-    gaussian(x)  = exp(-x * x / (2.0f * sigma * sigma));
+    gaussian(x) = exp(-x * x / (2.0f * sigma * sigma)) / (sqrt(2.f * IMP::PI) * sigma);
 
     Func image = BoundaryConditions::repeat_edge(state);
 
-    RVar domain(-radius, radius);
+    RDom domain(-radius, 2 * radius + 1);
+    RVar varDomain = domain[0];
     // Vertical Blur
-//    Func vertBlur;
-//    vertBlur(x, y) = gaussian(cast<int>(abs(domain))) * image(x, y + domain);
-//
-//    // Horizontal Blur
-//    Func horzBlur;
-//    horzBlur(x, y) = gaussian(cast<int>(abs(domain))) * image(x + domain, y);
-    Func result;
-    result(x, y) = image(x, y);
-    result.compile_to_file("gaussianBlur", {state, sigma});
+    Func vertBlur;
+    vertBlur(x, y) = gaussian(cast<int>(abs(domain))) * image(x, y + varDomain) / sum(gaussian(domain));
+    
+    // Horizontal Blur
+    Func horzBlur;
+    horzBlur(x, y) = gaussian(cast<int>(abs(domain))) * image(x + varDomain, y) / sum(gaussian(domain));
+    
+    // Convolute Horizontal and Vertical
+    Func finalImage;
+    finalImage(x, y) = IMP::convolution(image, vertBlur, 1, 2 * radius + 1) * IMP::convolution(stepImage,  horzBlur, 2 * radius + 1, 1);
+    finalImage.compile_to_file("gaussianBlur", {state, sigma});
 
     return 0;
 }
--- ImageProcessing/Utility/Convolution/Halide/convolution.h
+++ ImageProcessing/Utility/Convolution/Halide/convolution.h
@@ -9,9 +9,9 @@ namespace IMP {
 Func convolution(Func input, Func kernel, int width, int height) {
     Var x, y;
     Func convolute;
-    RDom horzDomain(0, width);
-    RDom vertDomain(0, height);
-    convolute(x, y) = input(x, y);
+    RDom horzDomain(-(width - 1)/2, width);
+    RDom vertDomain(-(height - 1)/2, height);
+    convolute(x, y) = input(x + horzDomain, y + vertDomain) * kernel(horzDomain, vertDomain);
     return convolute;
 }
 
